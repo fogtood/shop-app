c21178448a91bb2abdb38511be3fe105c618dba8
FJ-AKANDE <akandefortunatus2021@gmail.com>
2024-11-09 16:17:16 +0100

    chore: firebase installation

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD3LEesfqvdWYLAU2nU2cT5Xco-8EuTu0k",
+  authDomain: "shop-app-gt.firebaseapp.com",
+  projectId: "shop-app-gt",
+  storageBucket: "shop-app-gt.firebasestorage.app",
+  messagingSenderId: "860092036595",
+  appId: "1:860092036595:web:719c5cd43a2ddb5960ce86",
+};
+
+const app = initializeApp(firebaseConfig);
