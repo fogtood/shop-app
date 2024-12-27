53c91b852bf5140ab2d77c0f4f3b2eac3c7e08cb
FJ-AKANDE <akandefortunatus2021@gmail.com>
2024-12-27 05:33:30 +0100

    feat: refactor navbar and sidebar components to streamline user navigation and improve layout

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -2,18 +2,11 @@ import { Search, ShoppingCart, Menu } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
 import Button from "../button/button.component";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import ProfileDropdown from "../profile-dropdown/profile-dropdown.component";
 import Sheet from "../sheet/sheet.component";
 import Sidebar from "../sidebar/sidebar.component";
 import brand from "../../assets/brand.png";
-import defaultAvatar from "../../assets/defaultAvatar.jpg";
-import { IoMdMore } from "react-icons/io";
-import { FaUser } from "react-icons/fa";
-import { MdLogout } from "react-icons/md";
-import { logout } from "../../store/reducers/authSlice";
-import { signOutUser } from "../../utils/firebase/firebase.utils";
-import { toast } from "react-toastify";
 
 const navbarLinks = [
   {
@@ -38,12 +31,10 @@ const Navbar = () => {
   const navbar = useRef();
   const navigate = useNavigate();
   const location = useLocation();
-  const dispatch = useDispatch();
   const user = useSelector((state) => state.auth.user);
   const cart = useSelector((state) => state.cart.cartItems);
   const [isSheetOpen, setIsSheetOpen] = useState(false);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
 
   const scrollHandler = () => {
     if (navbar.current) {
@@ -64,26 +55,12 @@ const Navbar = () => {
   const closeSheet = () => setIsSheetOpen(false);
   const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
   const closeSidebar = () => setIsSidebarOpen(false);
-  const toggleUserMenu = () => setIsUserMenuOpen((prev) => !prev);
 
   // Format display name
   const formatDisplayName = (name) => {
     if (!name) return "";
     if (name.split(" ").length > 1) return name.split(" ")[0];
     return name.length > 16 ? name.slice(0, 16) + "..." : name;
-  };
-
-  const signOut = async () => {
-    try {
-      await signOutUser();
-      dispatch(logout());
-      navigate("/");
-      toggleUserMenu();
-      closeSidebar();
-    } catch (error) {
-      console.log(error);
-      toast.error("Error signing out");
-    }
   };
 
   return (
@@ -194,93 +171,12 @@ const Navbar = () => {
         </div>
       </nav>
       <Sheet isSheetOpen={isSheetOpen} closeSheet={closeSheet} />
-      <Sidebar isSidebarOpen={isSidebarOpen} closeSidebar={closeSidebar}>
-        <div className="flex flex-col h-full">
-          <div className="flex-1 flex flex-col gap-4">
-            {navbarLinks.map(({ name, url }, idx) => (
-              <NavLink
-                key={idx}
-                to={url}
-                className={({ isActive }) =>
-                  `${
-                    isActive ? "text-black font-bold" : "text-text"
-                  } text-sm font-medium hover:bg-gray-100 p-2 rounded-sm`
-                }
-                onClick={closeSidebar}
-              >
-                {name}
-              </NavLink>
-            ))}
-            {!user && (
-              <div className="flex flex-col gap-4">
-                <Button
-                  buttonType={"primary"}
-                  disabled={location.pathname === "/sign-up"}
-                  onClick={() => {
-                    navigate("/sign-up");
-                    closeSidebar();
-                  }}
-                >
-                  Sign Up
-                </Button>
-                <Button
-                  buttonType={"secondary"}
-                  disabled={location.pathname === "/sign-in"}
-                  onClick={() => {
-                    navigate("/sign-in");
-                    closeSidebar();
-                  }}
-                >
-                  Sign In
-                </Button>
-              </div>
-            )}
-          </div>
-          <div className="userC mt-auto mb-4">
-            {isUserMenuOpen && (
-              <div className="mb-2 p-2 bg-white shadow-lg rounded-md">
-                <Link
-                  to="/account"
-                  className="flex items-center justify-between whitespace-nowrap px-4 py-3 text-sm hover:bg-gray-100"
-                  onClick={() => {
-                    toggleUserMenu();
-                    closeSidebar();
-                  }}
-                >
-                  View Account
-                  <FaUser className="text-xl" />
-                </Link>
-
-                <button
-                  onClick={signOut}
-                  className="flex items-center justify-between w-full text-left px-4 py-3 text-sm border-t whitespace-nowrap hover:bg-gray-100"
-                >
-                  Sign Out
-                  <MdLogout className="text-xl text-red-400" />
-                </button>
-              </div>
-            )}
-            {user && (
-              <div className="flex justify-between">
-                <div className="flex items-center gap-2">
-                  <img
-                    src={user.avatar || defaultAvatar}
-                    alt="avatar"
-                    className="h-10 w-10 rounded-full"
-                  />
-                  <div>
-                    <p>{user.displayName}</p>
-                    <p className="text-text text-sm">{user.email}</p>
-                  </div>
-                </div>
-                <button type="button" onClick={toggleUserMenu}>
-                  <IoMdMore className="text-2xl text-text" />
-                </button>
-              </div>
-            )}
-          </div>
-        </div>
-      </Sidebar>
+      <Sidebar
+        navbarLinks={navbarLinks}
+        isSidebarOpen={isSidebarOpen}
+        closeSidebar={closeSidebar}
+        user={user}
+      />
     </>
   );
 };
--- src/components/sidebar/sidebar.component.jsx
+++ src/components/sidebar/sidebar.component.jsx
@@ -1,11 +1,15 @@
+import { NavLink, useNavigate } from "react-router-dom";
+import Button from "../button/button.component";
 import { X } from "lucide-react";
 
-const Sidebar = ({ isSidebarOpen, closeSidebar, children }) => {
+const Sidebar = ({ isSidebarOpen, closeSidebar, navbarLinks, user }) => {
+  const navigate = useNavigate();
+
   return (
     <div
       className={`fixed inset-0 z-50 transform ${
         isSidebarOpen ? "translate-x-0" : "-translate-x-full"
-      } transition-transform duration-300 ease-in-out bg-white shadow-lg flex flex-col h-screen`}
+      } w-64 transition-transform duration-300 ease-in-out bg-white shadow-lg flex flex-col h-screen`}
     >
       <div className="flex justify-between items-center p-4 border-b">
         <h2 className="text-lg font-medium">Menu</h2>
@@ -13,7 +17,50 @@ const Sidebar = ({ isSidebarOpen, closeSidebar, children }) => {
           <X />
         </button>
       </div>
-      <div className="flex-1 p-4 overflow-y-auto">{children}</div>
+      <div className="flex-1 p-4 overflow-y-auto">
+        <div className="flex flex-col h-full">
+          <div className="flex-1 flex flex-col gap-4">
+            {navbarLinks.map(({ name, url }, idx) => (
+              <NavLink
+                key={idx}
+                to={url}
+                className={({ isActive }) =>
+                  `${
+                    isActive ? "text-black font-bold" : "text-text"
+                  } text-sm font-medium hover:bg-gray-100 p-2 rounded-sm`
+                }
+                onClick={closeSidebar}
+              >
+                {name}
+              </NavLink>
+            ))}
+            {!user && (
+              <div className="flex flex-col gap-4">
+                <Button
+                  buttonType={"primary"}
+                  disabled={location.pathname === "/sign-up"}
+                  onClick={() => {
+                    navigate("/sign-up");
+                    closeSidebar();
+                  }}
+                >
+                  Sign Up
+                </Button>
+                <Button
+                  buttonType={"secondary"}
+                  disabled={location.pathname === "/sign-in"}
+                  onClick={() => {
+                    navigate("/sign-in");
+                    closeSidebar();
+                  }}
+                >
+                  Sign In
+                </Button>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
